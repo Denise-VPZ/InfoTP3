87aaa1e86cd2b5617bb7fa1da156d845980e10a8
Denise-VPZ <telavictoria2.0@gmail.com>
2025-05-19 00:14:50 -0300

    Add files via upload

---
+++ bmi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    float x, y;
+    printf("Ingrese su peso en kg:");
+    scanf("\n %f", &x);
+    printf("\nSu peso es: %.2f", x);
+    printf("\nIngrese su altura en metros:");
+    scanf("\n %f", &y);
+    printf("\nSu altura es: %.2f", y);
+    
+    float hcuadrado= y*y;
+    float IMC= x/hcuadrado;
+    
+    printf("\nSu IMC es: %f", IMC);
+    
+    printf("\n \nTABLA DE IMC \nPeso bajo: menor a 18.5 \nPeso Normal: entre 18.5 y 24.9 \nSobrepeso: entre 25 y 29.9 \nObesidad: de 30 en adelante");
+
+
+    return 0;
+}
